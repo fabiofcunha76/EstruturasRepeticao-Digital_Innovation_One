124ef57af1d00285d36c9f77cbdcca2749262484
fabiofcunha76 <83621272+fabiofcunha76@users.noreply.github.com>
2023-06-11 21:40:41 -0300

    Update Main.java
    Ajuste conteúdo arquivo.

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,6 +2,6 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-                System.out.println("Luciana só dorme!");
+                System.out.println("Exercicios bootcamp Kotlin da DIO!");
     }
 }
